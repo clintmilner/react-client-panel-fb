1189bbc1457efdab669d2c9b860fbe8c84c3eb46
Clint Milner <clinton.milner@rebasoft.net>
2018-09-27 08:03:05 +0100

    retrieved clients from firebase, custom loading component, calculated total owed and used getDerivedStateFromProps to add to component state

---
+++ src/components/layout/Loading.js
@@ -0,0 +1,22 @@
+/**
+ * User: clint
+ * Date: 27/09/2018
+ * Time: 07:50
+ *
+ * Rebasoft - Network Intelligence
+ */
+
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="fa-3x">
+            {/*<i className="fas fa-spinner fa-spin"></i>*/}
+            {/*<i className="fas fa-circle-notch fa-spin"></i>*/}
+            {/*<i className="fas fa-sync fa-spin"></i>*/}
+            <i className="fas fa-cog fa-spin"></i> Loading...
+            {/*<i className="fas fa-spinner fa-pulse"></i>*/}
+            {/*<i className="fas fa-stroopwafel fa-spin"></i>*/}
+        </div>
+    );
+};
